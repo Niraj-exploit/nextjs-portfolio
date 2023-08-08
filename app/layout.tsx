b0c9f32bd67eec25c975bef76c22451d7300e157
Niraj Paudel <77-bca.12@texasintl.edu.np>
2023-08-08 18:45:40 +0545

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "niraj-exploit",
+		template: "%s | niraj-exploit",
 	},
 	description: "Software engineer at upstash.com and founder of planetfall.io",
 	openGraph: {
-		title: "chronark.com",
+		title: "niraj-exploit",
 		description:
 			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
+		url: "#",
 		siteName: "chronark.com",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "#",
 				width: 1920,
 				height: 1080,
 			},
