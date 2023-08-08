2a64460e43c6ff3030ce97874cff9be4d29c00c5
Niraj Paudel <77-bca.12@texasintl.edu.np>
2023-08-08 17:22:44 +0545

    Updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/wwwNirajpaudel",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@wwwNirajpaudel",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:www.nirajpaudel@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "www.nirajpaudel@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/niraj-exploit",
 		label: "Github",
-		handle: "chronark",
+		handle: "niraj-exploit",
 	},
 ];
 
--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,14 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				niraj-exploit
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
+					Hi, my name is Niraj Paudel, 
+					{/* I'm building serverless and open source
 					solutions at{" "}
 					<Link
 						target="_blank"
@@ -54,7 +55,7 @@ export default function Home() {
 					>
 						unkey.dev
 					</Link>{" "}
-					at night.
+					at night. */}
 				</h2>
 			</div>
 		</div>
